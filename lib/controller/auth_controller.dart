ff0f6ac2afb036cf83689fdc1db01ab5e37b3d44
mahmed-developer <mahmed.dev2003@gmail.com>
2024-08-12 13:04:31 +0500

    Update auth_controller.dart

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -1,16 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 final authControllerProvider = StateNotifierProvider<AuthController, User?>(
-      (ref) => AuthController(FirebaseAuth.instance, FirebaseFirestore.instance),
+      (ref) => AuthController(FirebaseAuth.instance, FirebaseFirestore.instance,GoogleSignIn()),
 );
 
 class AuthController extends StateNotifier<User?> {
   final FirebaseAuth auth;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
+  GoogleSignIn googleSignIn;
 
-  AuthController(this.auth, this.firestore) : super(auth.currentUser);
+  AuthController(this.auth, this.firestore, this.googleSignIn) : super(auth.currentUser);
 
   Future<bool> signUp(String email, String password, String name ) async {
     try {
@@ -56,5 +58,13 @@ class AuthController extends StateNotifier<User?> {
   Future<void> signOut() async {
     await auth.signOut();
     state = null;
+  }
+  Future<void>signInWithGoogle() async {
+    final user = await googleSignIn.signIn();
+    final googleauth = await user!.authentication;
+    final credentials = GoogleAuthProvider.credential(
+        accessToken: googleauth.accessToken, idToken: googleauth.idToken);
+    await auth.signInWithCredential(credentials);
+    state = auth.currentUser;
   }
 }
